37fd8e63fac9ed231b180d7a420f5fa609c440fc
ariefnurputranto <arief@qiscus.co>
2018-09-25 15:57:11 +0700

    implement customChatInput for typing and send reply text

--- Qiscus/ChatInput/CustomChatInput.swift
+++ Qiscus/ChatInput/CustomChatInput.swift
@@ -9,6 +9,7 @@
 import UIKit
 import QiscusUI
 import QiscusCore
+import SwiftyJSON
 
 protocol CustomChatInputDelegate {
     func sendAttachment()
@@ -16,26 +17,75 @@ protocol CustomChatInputDelegate {
 
 class CustomChatInput: UIChatInput {
     
+    @IBOutlet weak var heightView: NSLayoutConstraint!
     @IBOutlet weak var sendButton: UIButton!
     @IBOutlet weak var attachButton: UIButton!
-    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var heightTextViewCons: NSLayoutConstraint!
+    @IBOutlet weak var textView: UITextView!
     var delegate : CustomChatInputDelegate? = nil
+    var replyData:CommentModel?
     
     override func commonInit(nib: UINib) {
         let nib = UINib(nibName: "CustomChatInput", bundle: Qiscus.bundle)
         super.commonInit(nib: nib)
-        textField.delegate = self
+        textView.delegate = self
+        textView.text = QiscusTextConfiguration.sharedInstance.textPlaceholder
+        textView.textColor = UIColor.lightGray
+        textView.font = Qiscus.sharedInstance.styleConfiguration.chatFont
+        
     }
     
     @IBAction func clickSend(_ sender: Any) {
-        guard let text = self.textField.text else {return}
-        if !text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+        guard let text = self.textView.text else {return}
+        if !text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty && text != QiscusTextConfiguration.sharedInstance.textPlaceholder {
+            var payload:JSON? = nil
             let comment = CommentModel()
-            comment.type = "text"
-            comment.message = text
+            if(replyData != nil){
+                var senderName = replyData?.username
+
+//                var payloadArray: [(String,Any)] = [
+//                    ("replied_comment_sender_email",replyData?.userEmail),
+//                    ("replied_comment_id", replyData?.id),
+//                    ("text", text),
+//                    ("replied_comment_message", replyData?.message),
+//                    ("replied_comment_sender_username", senderName),
+//                    ("replied_comment_payload", replyData?.payload)
+//                ]
+//
+                
+//                payload = JSON(dictionaryLiteral: payloadArray)
+//
+                comment.type = "reply"
+                comment.message = text
+                comment.payload = [
+                    "replied_comment_sender_email"       : replyData?.userEmail,
+                    "replied_comment_id" : Int((replyData?.id)!),
+                    "text"      : text,
+                    "replied_comment_message"   : replyData?.message,
+                    "replied_comment_sender_username" : senderName,
+                    "replied_comment_payload" : replyData?.payload,
+                    "replied_comment_type" : "text"
+                ]
+                //if reply.type == .location || reply.type == .contact {
+                //                   payloadArray.append(("replied_comment_type",reply.typeRaw))
+                //                }
+                self.replyData = nil
+            }else{
+               
+                comment.type = "text"
+                comment.message = text
+                
+            }
+            
             self.send(message: comment)
+           
         }
-        self.textField.text = ""
+        
+        
+        
+        self.textView.text = ""
+        textView.textColor = UIColor.lightGray
     }
     
     @IBAction func clickAttachment(_ sender: Any) {
@@ -43,12 +93,32 @@ class CustomChatInput: UIChatInput {
     }
 }
 
-extension CustomChatInput : UITextFieldDelegate {
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        self.typing(true)
+extension CustomChatInput : UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if(textView.text == QiscusTextConfiguration.sharedInstance.textPlaceholder){
+            textView.text = ""
+            textView.textColor = UIColor.black
+        }
     }
     
-    func textFieldDidEndEditing(_ textField: UITextField) {
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if(textView.text.isEmpty){
+            textView.text = QiscusTextConfiguration.sharedInstance.textPlaceholder
+            textView.textColor = UIColor.lightGray
+        }
         self.typing(false)
     }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        self.typing(true)
+        let fixedWidth = textView.frame.size.width
+        let newSize = textView.sizeThatFits(CGSize.init(width: fixedWidth, height: CGFloat(MAXFLOAT)))
+        if (newSize.height >= 40 && newSize.height <= 100) {
+            self.heightTextViewCons.constant = newSize.height
+            self.heightView.constant = newSize.height + 10.0
+            
+        }
+        if (newSize.height >= 100) { self.textView.isScrollEnabled = true }
+    }
+    
 }
--- Qiscus/Extension/ViewController.swift
+++ Qiscus/Extension/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import QiscusUI
 
 extension UIViewController {
 
