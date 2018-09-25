fbd9a9152b6386625f0a894b5723fe0dc23a2f6e
ariefnurputranto <arief@qiscus.co>
2018-09-25 15:53:51 +0700

    implement QUIBaseChatCell

--- Qiscus/QUIBaseChatCell.swift
+++ Qiscus/QUIBaseChatCell.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+import QiscusCore
+import QiscusUI
+
+extension UIBaseChatCell {
+    
+    open func setMenu() {
+        let reply = UIMenuItem(title: "Reply", action: #selector(reply(_:)))
+        
+        UIMenuController.shared.menuItems = [reply]
+        UIMenuController.shared.update()
+        
+    }
+    
+    @objc open func reply(_ send:AnyObject){
+        QiscusNotification.publishDidClickReply(message: self.comment!)
+    }
+}
+
+
